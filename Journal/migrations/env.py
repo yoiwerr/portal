df9b9e59cfa4316f8220c3b0c96c62d9b43985ba
yoiwerr <yoiwerr@163.com>
2026-08-25 23:23:14 +0800

    fix Journal Alembic online connection

--- Journal/migrations/env.py
+++ Journal/migrations/env.py
@@ -9,7 +9,8 @@ def run_migrations_offline():
  context.configure(url=config.get_main_option("sqlalchemy.url"),target_metadata=target_metadata,literal_binds=True,compare_type=True)
  with context.begin_transaction(): context.run_migrations()
 def run_migrations_online():
- with engine_from_config(config.get_section(config.config_ini_section),prefix="sqlalchemy.",poolclass=pool.NullPool) as connection:
+ connectable=engine_from_config(config.get_section(config.config_ini_section),prefix="sqlalchemy.",poolclass=pool.NullPool)
+ with connectable.connect() as connection:
   context.configure(connection=connection,target_metadata=target_metadata,compare_type=True)
   with context.begin_transaction(): context.run_migrations()
 if context.is_offline_mode(): run_migrations_offline()
